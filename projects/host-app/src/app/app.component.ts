4932caaa81125004014d73dda9790672d9e6bbfc
Ravi Kant Prasad <ravikantp@byteridge.com>
2025-01-27 18:51:43 +0530

    MUI added

--- projects/host-app/src/app/app.component.ts
+++ projects/host-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatSlideToggleModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
