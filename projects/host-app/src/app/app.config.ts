1ec3ed75e6e289e3ccec8816702fc2784eece51c
Ravi Kant Prasad <ravikantp@byteridge.com>
2025-01-28 17:44:38 +0530

    missing imports added

--- projects/host-app/src/app/app.config.ts
+++ projects/host-app/src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
+    provideNativeDateAdapter(),
   ],
 };
