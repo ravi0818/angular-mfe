e6cd97bb5cd83145d6f5c92b0ae1f023b8c7d5f8
Ravi Kant Prasad <rkprasad0818@gmail.com>
2025-02-05 17:14:39 +0000

    removed unused services

--- projects/common/src/public-api.ts
+++ projects/common/src/public-api.ts
@@ -2,14 +2,11 @@
  * Public API Surface of common
  */
 
-export * from './lib/common.service';
-export * from './lib/common.component';
 export * from './lib/alert-dialog/alert-dialog.component';
 export * from './lib/confirm-dialog/confirm-dialog.component';
 export * from './lib/custom-dialog/custom-dialog.component';
 export * from './lib/table/table.component';
 
-export * from './lib/interface/product';
 export * from './lib/services/cart.actions';
 export * from './lib/services/cart.reducer';
 export * from './lib/services/cart.selectors';
